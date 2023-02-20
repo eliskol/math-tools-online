d5e74d80a696cd8f0da7b7dd124387bb51b66478
Elias <75465281+eliskol@users.noreply.github.com>
2023-02-20 04:19:47 +0000

    super basic implementation! currently alerts distribution.atPoint value

--- prob-dists/ui.js
+++ prob-dists/ui.js
@@ -4,10 +4,52 @@ import { NegativeBinomialDistribution } from "./distributions/negative-binomial.
 const DISTRIBUTIONS = [BinomialDistribution, NegativeBinomialDistribution];
 
 let distributionSelector = document.querySelector('#distributionSelector');
+let distributionInputContainer = document.querySelector('#distributionInputContainer')
+let mainDistributionInput = document.querySelector('#mainDistributionInput');
+let currentDistributionClass;
+let distribution;
 
-for (const DISTRIBUTION of DISTRIBUTIONS) {
+mainDistributionInput.addEventListener('input', () => {
+    if (!currentDistributionClass) return;
+    alert(distribution.atPoint(mainDistributionInput.value));
+});
+
+const createDistributionOptionElement = (distribution, indexOfDistribution) => {
     let distributionOption = document.createElement('option');
-    distributionOption.value = DISTRIBUTION.name;
-    distributionOption.textContent = DISTRIBUTION.name;
+    distributionOption.value = indexOfDistribution;
+    distributionOption.textContent = distribution.name;
+    return distributionOption;
+};
+
+for (const distribution of DISTRIBUTIONS) {
+    let distributionOption = createDistributionOptionElement(distribution, DISTRIBUTIONS.indexOf(distribution));
     distributionSelector.appendChild(distributionOption);
 }
+
+const createDistributionInputElement = (inputName) => {
+        let inputElement = document.createElement('input');
+        inputElement.placeholder = inputName;
+        inputElement.type = 'number';
+        inputElement.className = 'distributionInitializationInput';
+        inputElement.addEventListener('input', () => {
+            let distributionInputElements = document.querySelectorAll('.distributionInitializationInput');
+            let distributionInitializationInputArray = [...distributionInputElements].map((element) => element.value);
+            createDistribution(distributionInitializationInputArray);
+        });
+        return inputElement;
+};
+
+
+
+const createDistribution = (distributionInitializationInputArray) => {
+    distribution = new currentDistributionClass(...distributionInitializationInputArray);
+}
+
+distributionSelector.addEventListener('change', () => {
+    let distributionSelected = DISTRIBUTIONS[distributionSelector.value];
+    currentDistributionClass = distributionSelected;
+    for (const inputName of distributionSelected.inputs.reverse()) { // reversed because prepend will reverse back to correct order
+        let inputElement = createDistributionInputElement(inputName);
+        distributionInputContainer.prepend(inputElement);
+    }
+});
